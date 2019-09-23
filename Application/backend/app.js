95e3b73bc3e6c31bcff4f619c66c962ef8533b4d
Persha Pakdast <pepak16@student.sdu.dk>
2019-09-23 17:34:27 +0200

    Insert works, but fetch almost works!

--- Application/backend/app.js
+++ Application/backend/app.js
@@ -1,9 +1,10 @@
 var con = require('./db');
 
-function insertUser() {
+//Insert user data into database using their usernames, emails and user passwords.
+function insertUser(un, um, up) {
     con.connect(function(err) {
         console.log("Connected!");
-        var sql = "INSERT INTO Users (UserName, UserMail, UserPass) VALUES ('Company Inc23123', 'Highwa213@sf.com', '123123')";
+        var sql = "INSERT INTO Users (UserName, UserMail, UserPass) VALUES "+"('"+un+"','"+um+"','"+up+"')";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("1 record inserted");
@@ -11,13 +12,28 @@ function insertUser() {
     });
 }
 
-function fetchUser() {
+// Select specific user using their usernames and passwords and check whether they exists.
+// If the username and password matches, it will also mean that the user exists and hence return true,
+// if not, then return false.
+function fetchUser(un,up) {
     con.connect(function(err) {
         console.log("Connected!");
-        var sql = "SELECT UserName, UserPass FROM Users WHERE UserName = 'Company Incs' AND UserPass = '123123'";
+        var sql = "SELECT UserName, UserPass FROM Users WHERE UserName = "+"'"+un+"' AND UserPass = '"+up+"'";
         con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
+            if (err) {
+                //return false;
+                console.log(err);
+                throw err;
+            } else {
+                if (result[0].UserName === un) {
+                    console.log(result[0].UserName);
+                    console.log(err);
+                    return true;
+                } else {
+                    return false;
+                }
+                //console.log(result[0].UserName);
+            }
         });
     });
 }
--- Application/backend/test.js
+++ Application/backend/test.js
@@ -1,3 +1,10 @@
 const appclass = require('./app');
 
-appclass.fetchUser();
+//appclass.insertUser('persha95','persha95@gmail.com','123123pp');
+//console.log('insert user works!');
+
+if (appclass.fetchUser('persha95','123123pp')) {
+    console.log("true");
+} else {
+    console.log("false");
+}
