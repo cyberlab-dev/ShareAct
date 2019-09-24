95eafa209c13b724454e7f08d2172bb13e0b5232
Unknown <mikkelschmoede@gmail.com>
2019-09-24 12:51:14 +0200

    Login, Register and routing
    redid routing, and added register and login

--- Application/js/routes.js
+++ Application/js/routes.js
@@ -6,13 +6,31 @@ function loadView(view) {
             document.getElementById("Content").innerHTML = this.responseText;
         }
     };
-    if (viewPath == 'chat') {
+    /*if (viewPath == 'chat') {
         xhttp.open("GET", "views/chat.html", true);
         xhttp.send();
     } else if (viewPath == 'profile') {
         xhttp.open("GET", "views/profile.html", true);
         xhttp.send();
     } else {
+        xhttp.open("GET", "views/home.html", true);
+        xhttp.send();
+    } */
+
+    switch(viewPath) {
+    case 'chat':
+        xhttp.open("GET", "views/chat.html", true);
+        xhttp.send();
+        break;
+    case 'profile':
+        xhttp.open("GET", "views/profile.html", true);
+        xhttp.send();
+        break;
+    case 'reg':
+        xhttp.open("GET", "views/register.html", true);
+        xhttp.send();
+        break;
+    default:
         xhttp.open("GET", "views/home.html", true);
         xhttp.send();
     }
